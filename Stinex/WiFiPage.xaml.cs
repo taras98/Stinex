ed2071ae5f49d503fab1741cb9b8b714511a8d7c
Taras Vynar <vynar.taras@gmail.com>
2020-02-25 09:24:06 +0200

    post

--- Stinex/WiFiPage.xaml.cs
+++ Stinex/WiFiPage.xaml.cs
@@ -11,6 +11,8 @@ using Android.Net.Wifi;
 using System.Net;
 using System.IO;
 using System.Threading;
+using HtmlAgilityPack;
+using RestSharp;
 
 namespace Stinex
 {
@@ -23,25 +25,60 @@ namespace Stinex
         }
         void ConnectClicked(object sender, EventArgs e)
         {
-            var wifiManager = (WifiManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.WifiService);
-            var current = Connectivity.NetworkAccess ;
-
-            Con.Text = current.ToString();
-            Thread.Sleep(2000);
-            wifiManager.DisableNetwork(wifiManager.ConnectionInfo.NetworkId);
-            Con.Text = wifiManager.DisableNetwork(wifiManager.ConnectionInfo.NetworkId).ToString();
-
             /*
-            string site = "http://192.168.1.1?SSID=valera&Password=lox";
+                        var wifiManager = (WifiManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.WifiService);
+                        wifiManager.Disconnect();
 
-            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(site);
-            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                        string ssid = "stinex_v1.0";
+                        string password = "12345678";
+                        var formattedSsid = $"\"{ssid}\"";
+                        var formattedPassword = $"\"{password}\"";
 
-            using (StreamReader stream = new StreamReader(
-                 resp.GetResponseStream(), Encoding.UTF8))
-            {
-            }
-            */
-        }
+                        var wifiConfig = new WifiConfiguration
+                        {
+                            Ssid = formattedSsid,
+                            PreSharedKey = formattedPassword
+                        };
+                        var addNetwork = wifiManager.AddNetwork(wifiConfig);
+                        var network = wifiManager.ConfiguredNetworks
+                             .FirstOrDefault(n => n.Ssid == formattedSsid);
+                        var enableNetwork = wifiManager.EnableNetwork(network.NetworkId, true);
+                        if (network == null)
+                        {
+                            return;
+                        }
+                        string site = "http://192.168.1.1";
+
+                        string site2 = "http://192.168.1.1/par?ssid=hello&pas=stinex";
+                        var web = new HtmlAgilityPack.HtmlWeb();
+                        HtmlDocument doc = web.Load(site);
+
+                        HtmlDocument doc2 = web.Load(site2);
+                        var a = doc2.ParsedText;
+
+                        WebRequest request = WebRequest.Create(site);
+                        WebResponse response = request.GetResponse();
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                string line = "";
+                                while ((line = reader.ReadLine()) != null)
+                                {
+                                    Console.WriteLine(line);
+                                }
+                            }
+                        }
+                        response.Close();
+                        */
+            var client = new RestClient("http://192.168.7.239/index.php");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            var request = new RestRequest("resource/{id}");
+            request.AddParameter("id", "Hello");
+            
+            var response = client.Post(request);
+            var content = response.Content; // raw content as string
+          }
+        
     }
 }
