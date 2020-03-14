0d693e63f8ade44445b3189ef41e46707a0a81d0
Taras Vynar <vynar.taras@gmail.com>
2020-03-14 14:31:29 +0200

    1403

---
+++ Stinex/ObtainData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stinex
+{
+    public class ObtainData
+    {
+        public double temp_room { get; set; }
+        public double temp_panel { get; set; }
+        public int heat_status { get; set; }
+    }
+}
--- Stinex/WiFiPage.xaml.cs
+++ Stinex/WiFiPage.xaml.cs
@@ -13,72 +13,137 @@ using System.IO;
 using System.Threading;
 using HtmlAgilityPack;
 using RestSharp;
+using Newtonsoft.Json;
 
 namespace Stinex
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WiFiPage : ContentPage
     {
+        ObtainData data = new ObtainData();
         public WiFiPage()
         {
             InitializeComponent();
         }
         void ConnectClicked(object sender, EventArgs e)
         {
+
+            var wifiManager = (WifiManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.WifiService);
+
+            //string ssid = "stinex";
+            //string password = "stinex2908";
+            //var formattedSsid = $"\"{ssid}\"";
+            //var formattedPassword = $"\"{password}\"";
+
+            //var wifiConfig = new WifiConfiguration
+            //{
+            //    Ssid = formattedSsid,
+            //    Bssid = formattedSsid,
+            //    PreSharedKey = formattedPassword
+            //};
+            //var addNetwork = wifiManager.AddNetwork(wifiConfig);
+            //var network = wifiManager.ConfiguredNetworks
+            //     .FirstOrDefault(n => n.Ssid == formattedSsid);
+            //IList<WifiConfiguration> myWifi = wifiManager.ConfiguredNetworks;
+            //wifiManager.Disconnect();
+            //wifiManager.EnableNetwork(myWifi.FirstOrDefault(x => x.Ssid.Contains(ssid)).NetworkId, true);
+            //wifiManager.Reconnect();
+            //if (network == null)
+            //{
+            //    return;
+            //}
+            //wifiManager.Disconnect();
+
+            //var enableNetwork = wifiManager.EnableNetwork(network.NetworkId, true);
+
             /*
-                        var wifiManager = (WifiManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.WifiService);
-                        wifiManager.Disconnect();
+            string site = "http://192.168.1.1";
 
-                        string ssid = "stinex_v1.0";
-                        string password = "12345678";
-                        var formattedSsid = $"\"{ssid}\"";
-                        var formattedPassword = $"\"{password}\"";
+            string site2 = "http://192.168.1.1/par?ssid=hello&pas=stinex";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc = web.Load(site);
 
-                        var wifiConfig = new WifiConfiguration
-                        {
-                            Ssid = formattedSsid,
-                            PreSharedKey = formattedPassword
-                        };
-                        var addNetwork = wifiManager.AddNetwork(wifiConfig);
-                        var network = wifiManager.ConfiguredNetworks
-                             .FirstOrDefault(n => n.Ssid == formattedSsid);
-                        var enableNetwork = wifiManager.EnableNetwork(network.NetworkId, true);
-                        if (network == null)
-                        {
-                            return;
-                        }
-                        string site = "http://192.168.1.1";
+            HtmlDocument doc2 = web.Load(site2);
+            var a = doc2.ParsedText;
 
-                        string site2 = "http://192.168.1.1/par?ssid=hello&pas=stinex";
-                        var web = new HtmlAgilityPack.HtmlWeb();
-                        HtmlDocument doc = web.Load(site);
+            WebRequest request = WebRequest.Create(site);
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            response.Close();
+            */
+            var a = POST("http://10.10.10.1:10893/scan");
+            //  name.Text = "Aviable network: " + a.Content.Replace(",", " ");
+            value.Text += a.Content;
 
-                        HtmlDocument doc2 = web.Load(site2);
-                        var a = doc2.ParsedText;
+            var b = POST("http://10.10.10.1:10893/dev_id");
+            string devid = b.Content;
+            value.Text += b.Content;
 
-                        WebRequest request = WebRequest.Create(site);
-                        WebResponse response = request.GetResponse();
-                        using (Stream stream = response.GetResponseStream())
-                        {
-                            using (StreamReader reader = new StreamReader(stream))
-                            {
-                                string line = "";
-                                while ((line = reader.ReadLine()) != null)
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                        }
-                        response.Close();
-                        */
-            var client = new RestClient(path.Text);
-            // client.Authenticator = new HttpBasicAuthenticator(username, password);
-            var request = new RestRequest("resource/{id}");
-            request.AddParameter(name.Text, value.Text);
+            var c = POSTparam("http://10.10.10.1:10893/data", a.Content.Replace(",", ""), "mel@sk1995");
+            var res = c.Content;
+            value.Text += c.Content;
+
+           //wifiManager.Disconnect();
             
-            var response = client.Post(request);
-            var content = response.Content; // raw content as string
-          }
-        
+
+            //var client = new RestClient(path.Text);
+            //// client.Authenticator = new HttpBasicAuthenticator(username, password);
+            //var request = new RestRequest("resource/{id}");
+            //request.AddParameter(name.Text, value.Text);
+
+            //var response = client.Post(request);
+            //var content = response.Content; // raw content as string
+        }
+        public IRestResponse POST(string url)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            return client.Execute(request);
+        }
+        public IRestResponse POSTparam(string url, string ssid, string pass)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("ssid", ssid);
+            request.AddParameter("password", pass);
+            return client.Execute(request);
+        }
+        public IRestResponse GETdevid(string url, string devid)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("dev_id", devid);
+            var response = client.Execute(request);
+            return response;
+        }
+        public IRestResponse GETsenddata(string url, string devid)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("dev_id", devid);
+            request.AddParameter("heat_control", 1);
+            request.AddParameter("need_temp_room", 22);
+            request.AddParameter("need_temp_panel", 60);
+            var response = client.Execute(request);
+            return response;
+        }
+
+        private void NextCon_Clicked(object sender, EventArgs e)
+        {
+            var d = GETdevid("http://213.174.0.16:12080/is_connect.php", "5C:CF:7F:3C:FF:E6");
+            data = JsonConvert.DeserializeObject<ObtainData>(d.Content);
+            value.Text += data.temp_panel.ToString() + data.temp_room.ToString() + data.heat_status.ToString();
+            var v = GETsenddata("http://213.174.0.16:12080/is_connect.php", "5C:CF:7F:3C:FF:E6");
+        }
     }
 }
